c07f378d2291c6f1a6760320b310e7565c2bfba4
Nhat Toan <toanln@cyfeer.com>
2019-06-17 18:10:16 +0700

    config server for deploy

--- utils/helper/buildEmailLink.js
+++ utils/helper/buildEmailLink.js
@@ -1,6 +1,9 @@
 import { PORT } from '../../config';
 
 exports.verifyEmailLink = (token, email) => {
-  const link = `http://localhost:${PORT}/verify/${token}/verify-email?email=${email}`;
-  return link;
+  if (process.env.NODE_ENV = 'production') {
+    return `https://nore-chatting-server.herokuapp.com/verify/${token}/verify-email?email=${email}`;
+  } else {
+    return `http://localhost:${PORT}/verify/${token}/verify-email?email=${email}`;
+  }
 }
